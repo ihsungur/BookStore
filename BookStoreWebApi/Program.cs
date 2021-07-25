fe02dda37a6e6b5aac320ea57273896bb6c1ecc0
ihsungur <78755810+ihsungur@users.noreply.github.com>
2021-07-25 15:47:07 +0300

    Revert "Revert "Revert "entityframework core eklendi"""
    This reverts commit fb89fa28ff54c1f7426101193334429cefd36c56.

--- BookStoreWebApi/Program.cs
+++ BookStoreWebApi/Program.cs
@@ -2,10 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BookStoreWebApi.DbOperations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -15,13 +13,7 @@ namespace BookStoreWebApi
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                DataGenerator.Initialize(services);
-            }
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
